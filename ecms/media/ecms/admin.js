0eec26edb61ddbbd5f459870bdafeaab2a30c7e4
Diederik van der Boor <vdboor@edoburu.nl>
2011-04-07 23:58:01 +0200

    Clean up JavaScript for admin  tab switching
    Applied concepts from "Clean Code".
    Let functions do simple tasks,

--- ecms/media/ecms/admin.js
+++ ecms/media/ecms/admin.js
@@ -14,10 +14,8 @@
   try { document.execCommand("BackgroundImageCache", false, true); }
   catch(exception) {}
 
-  var ajax_root = location.href.substring(0, location.href.indexOf('/cmsobject/') + 11);
-
   // Public functions
-  var regions   = [];   // [ 'main', 'sidebar', .. ]
+  var regions   = [];   // [ { key: 'main', title: 'Main', role: 'm' }, { key: 'sidebar', ...} ]
   var formsets  = [];   // [ { name: "somename_set, auto_id: "id_%s" }, .. ]
   window.ecms_admin = {
     'setRegions':  function(data) { regions   = data; }
@@ -27,161 +25,162 @@
   // Global vars
   var dom_formsets = {};  // { tab: DOM, items: [ item1, item2 ] }
   var has_load_error = false;
+  var ajax_root = location.href.substring(0, location.href.indexOf('/cmsobject/') + 11);
 
   // Constants
-  var REGION_ROLE_MAIN = 'm';
+  var REGION_ROLE_MAIN    = 'm';
   var REGION_ROLE_SIDEBAR = 's';
   var REGION_ROLE_RELATED = 'r';
 
+
+  // -------- Init code ------
 
   /**
    * Main init code
    */
   function onReady()
   {
-    // Bind events
+    // Simple events
     $("#ecms-tabnav a").click( onTabClick );
 
-    // Read DOM data
-    read_dom_formsets();
-
-    // Set layout
+    // Init layout selector
     var layout_selector = $("#id_layout");
+    select_single_option(layout_selector);
     layout_selector.change( onLayoutChange );
 
-    // Select default layout of there is only one.
-    var options = layout_selector[0].options;
-    if( ( options.length == 0 )
-     || ( options.length == 2 && options[0].value == "" ) )
+    // Init items in tabs
+    read_dom_formsets();
+    if( layout_selector.val() != 0 && $("#ecms-tabbar").is(":hidden"))
     {
-      layout_selector.val( options[ options.length - 1 ] .value );
-    }
-
-    if( layout_selector.val() != 0
-     && $("#ecms-tabbar").is(":hidden"))
-    {
-      // At firefox refresh, the form field value was restored,
-      // so make sure the state is in sync.
+      // At Firefox refresh, the form field value was restored,
+      // Sync the tab content by fetching the data.
       layout_selector.change();
     }
     else
     {
+      // Normal init, use what we have.
       organize_formset_items();
     }
   }
 
 
+  /**
+   * Read all the DOM formsets into the "dom_formsets" variable.
+   * This information is used in this library to lookup formsets.
+   */
   function read_dom_formsets()
   {
+    var i, region_key, inputs, ordering;
+
+    // Find all formset items.
+    // Split them by the region they belong to.
     var formsets = $("#inlines > div[id$=_set]");
-    var items = formsets.children("div:not(.header)");
-    for(var i = 0; i < items.length; i++)
+    var fs_items = formsets.children("div:not(.header)");
+    for(i = 0; i < fs_items.length; i++)
     {
-      var item = items.eq(i);
+      // Get formset DOM elements
+      var fs_item = fs_items.eq(i);
+      inputs      = fs_item.find("input");
+      region_key  = inputs.filter("[name$=-region]").addClass("ecms-input-region").val();
+      ordering    = inputs.filter("[name$=-ordering]").addClass("ecms-input-ordering").val();
 
-      // Get inputs
-      var inputs   = item.find("input");
-      var region   = inputs.filter("[name$=-region]").addClass("ecms-input-region").val();;
-      var ordering = inputs.filter("input[name$=-ordering]").addClass("ecms-input-ordering").val();
-
-      // Create administration for first item.
-      if(! dom_formsets[region])
-      {
-        dom_formsets[region] = {
-          key:   region
-        , items: []
-        };
-      }
+      // Auto create, based on what's found at the page.
+      if(!dom_formsets[region_key])
+        dom_formsets[region_key] = { key: region_key, items: [] };
 
       // Append item to administration
-      if(!dom_formsets[region].items[ordering])
-        dom_formsets[region].items[ordering] = item;
+      if(!dom_formsets[region_key].items[ordering])
+        dom_formsets[region_key].items[ordering] = fs_item;
     }
   }
 
 
+  /**
+   * If a selectbox has only one choice, enable it.
+   */
+  function select_single_option(selectbox)
+  {
+    var options = selectbox[0].options;
+    if( ( options.length == 1 )
+     || ( options.length == 2 && options[0].value == "" ) )
+    {
+      selectbox.val( options[ options.length - 1 ].value );
+    }
+  }
+
+
+  // -------- Organizing items in tabs ------
+
+  /**
+   * Move all formset items to their appropriate tabs.
+   */
   function organize_formset_items()
   {
-    // Get default region.
-    var default_region_id = get_region(REGION_ROLE_MAIN, 1);
+    var default_region_key = get_region_for_role(REGION_ROLE_MAIN, 1);  // Use first main block in case region is not filled in.
+
+    // Count number of seen tabs per role.
     var roles_seen = {};
+    for(var i in regions)
+      roles_seen[regions[i].key] = 0;
 
     // Move all items to the tabs.
-    for(var region_id in dom_formsets)
+    for(var region_key in dom_formsets)
     {
-      region = dom_formsets[region_id];
+      var region = dom_formsets[region_key];
+      roles_seen[region.role]++;
 
-      if( region_id == '' )
-      {
-        region_id = default_region_id;
-      }
-      else
-      {
-        if(roles_seen[region_id] == null)
-          roles_seen[region_id] = 1;
-        else
-          roles_seen[region_id]++;
-      }
+      if( region.items.length == 0)
+        continue;
 
-      // Use orphaned tab for separate content.
-      var tab = $("#tab-region-" + region_id);
+      // Find the tab.
+      // If the template designer used the same "key", the tab contents is migrated.
+      // Otherwise, a fallback tab is found that is used for the same role (it's purpose on the page).
+      var tab = $("#tab-region-" + (region_key || default_region_key));
       if( tab.length == 0 )
       {
-        // Find a different tab, based on the role.
-        // This role exists to assist in migrations.
-        fallback_region_id = get_region(region.role || REGION_ROLE_MAIN, roles_seen[region.key]);
-        if( fallback_region_id )
-        {
-          tab = $("#tab-region-" + fallback_region_id);
-          region_id = fallback_region_id;
-        }
-
-        if( tab.length == 0 )
-        {
-          // Fallback to special tab for orphans
-          $("#ecms-tabnav-orphaned").css("display", "inline");
-          tab = $("#tab-orphaned");
-        }
+        var last_occurance = roles_seen[region.role];
+        tab = get_fallback_tab(region.role, last_occurance);
       }
 
-      // Move items to tab
-      if( region.items.length > 0 )
-      {
-        tab.children(".ecms-region-empty").hide();
-
-        // Move all items to that tab.
-        // Restore item values upon restoring fields.
-        for(var ordering in region.items)
-        {
-          var item   = region.items[ordering];
-          var itemId = item.attr("id");
-
-          // Remove the item.
-          disable_wysiwyg(item);
-          var values = get_input_values(item);
-          tab.append( item.remove() );
-
-          // Fetch the node reference as it was added to the DOM.
-          item = tab.children("#" + itemId);
-          region.items[ordering] = item;
-
-          // Re-enable the item
-          set_input_values(item, values);
-          enable_wysiwyg(item);
-        }
-      }
+      // Fill the tab
+      tab.children(".ecms-region-empty").hide();
+      move_items_to_tab(region, tab);
     }
+  }
 
-    // In case a previous set of tabs was invalidated,
-    // they can be removed now. The sub items are migrated.
-    cleanup_old_tabs();
+
+  /**
+   * Move the items of one region to the given tab.
+   */
+  function move_items_to_tab(region, tab)
+  {
+    // Move all items to that tab.
+    // Restore item values upon restoring fields.
+    for(var ordering in region.items)
+    {
+      var item   = region.items[ordering];
+      var itemId = item.attr("id");
+
+      // Remove the item.
+      disable_wysiwyg(item);
+      var values = get_input_values(item);
+      tab.append( item.remove() );
+
+      // Fetch the node reference as it was added to the DOM.
+      item = tab.children("#" + itemId);
+      region.items[ordering] = item;
+
+      // Re-enable the item
+      set_input_values(item, values);
+      enable_wysiwyg(item);
+    }
   }
 
 
   /**
    * Find the desired region, including the preferred occurrence of it.
    */
-  function get_region(role, preferredNr)
+  function get_region_for_role(role, preferredNr)
   {
     var candidate = null;
     var itemNr = 0;
@@ -200,6 +199,33 @@
 
     return candidate;
   }
+
+
+  /**
+   * Get a fallback tab to store an orphaned item.
+   */
+  function get_fallback_tab(role, last_known_nr)
+  {
+    // Find the last region which was also the same role.
+    var tab = [];
+    var fallback_region_id = get_region_for_role(role || REGION_ROLE_MAIN, last_known_nr);
+    if( fallback_region_id )
+    {
+      tab = $("#tab-region-" + fallback_region_id);
+    }
+
+    // If none exists, reveal the tab for orphaned items.
+    if( tab.length == 0 )
+    {
+      $("#ecms-tabnav-orphaned").css("display", "inline");
+      tab = $("#tab-orphaned");
+    }
+
+    return tab;
+  }
+
+
+  // -------- Item data transfer ------
 
 
   function get_input_values(root)
@@ -261,31 +287,8 @@
     }
   }
 
-  function invalidate_tabs()
-  {
-    // Invalidate tab titles
-    $("#ecms-tabnav-loading").show();
-    $("#ecms-tabnav-orphaned").hide();
-    $("#ecms-tabnav > li.ecms-region").remove();
 
-    var tabmain = $("#ecms-tabmain");
-    var height = tabmain.height();
-    if( height )
-    {
-      tabmain.css("height", height + "px");  // set fixed height to avoid scrollbar/footer flashing.
-    }
-    tabmain.children(".ecms-region-tab").removeClass("ecms-region-tab").addClass("ecms-region-oldtab").attr("id",null).hide();
-  }
-
-  function cleanup_old_tabs()
-  {
-    var tabmain = $("#ecms-tabmain");
-    tabmain.children(".ecms-region-oldtab").remove();
-
-    // After children height recalculations / wysiwyg initialisation, restore auto height.
-    setTimeout( function() { tabmain.css("height", ''); }, 100 );
-  }
-
+  // -------- Layout switching ------
 
   /**
    * The layout has changed.
@@ -300,7 +303,7 @@
     }
 
     // Disable content
-    invalidate_tabs();
+    hide_all_tabs();
 
     if( event.originalEvent )
     {
@@ -357,14 +360,48 @@
     tab_links.click( onTabClick );
 
     // Migrate formset items.
+    // The previous old tabs can be removed afterwards.
     organize_formset_items();
+    remove_old_tabs();
 
     // Activate first if none active.
-    // Put after organize, so orphans tab might be visible
+    // This needs to happen after organize, so orphans tab might be visible
     if( $("#ecms-tabnav > li.active:visible").length == 0 )
       tab_links.eq(0).click();
   }
 
+
+  function hide_all_tabs()
+  {
+    // Replace tab titles with loading sign.
+    $("#ecms-tabnav-loading").show();
+    $("#ecms-tabnav-orphaned").hide();
+    $("#ecms-tabnav > li.ecms-region").remove();
+
+    // set fixed height to avoid scrollbar/footer flashing.
+    var tabmain = $("#ecms-tabmain");
+    var height = tabmain.height();
+    if( height )
+    {
+      tabmain.css("height", height + "px");
+    }
+
+    // Hide and mark as old.
+    tabmain.children(".ecms-region-tab").removeClass("ecms-region-tab").addClass("ecms-region-oldtab").attr("id",null).hide();
+  }
+
+
+  function remove_old_tabs()
+  {
+    var tabmain = $("#ecms-tabmain");
+    tabmain.children(".ecms-region-oldtab").remove();
+
+    // After children height recalculations / wysiwyg initialisation, restore auto height.
+    setTimeout( function() { tabmain.css("height", ''); }, 100 );
+  }
+
+
+  // -------- Basic tab events ------
 
   /**
    * Tab button click
