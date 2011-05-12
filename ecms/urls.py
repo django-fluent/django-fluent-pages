21e011d5f52478c91d019b19a155b3320b807942
Diederik van der Boor <vdboor@edoburu.nl>
2011-05-12 08:29:21 +0200

    Allow redirecting to the admin by padding @admin to the URL

--- ecms/urls.py
+++ ecms/urls.py
@@ -1,10 +1,26 @@
+"""
+The URLs to serve the CMS.
+
+They can be included using:
+
+    urlpatterns += patterns('',
+        url(r'', include('ecms.urls'))
+    )
+
+The following named URLs are defined:
+  - ecms-admin-redirect     - An redirect to the admin.
+  - ecms-page               - Display of a page.
+
+By Appending @admin to an URL, the request will be redirected to the admin URL of the page.
+"""
 from django.conf.urls.defaults import *
-from ecms.views.cmspageview import CmsPageView
+from ecms.views.cmspageview import CmsPageView, CmsPageAdminRedirect
 
 # The URLs of the cmspage are forced to end with a slash,
 # so django will redirect /admin will redirect to /admin/.
 # The same trick also needs to be used in the main site
 # which includes this file. Otherwise a rule matched after all.
 urlpatterns = patterns('ecms.views',
-    url(r'^$|^(?P<path>.*/)$', CmsPageView.as_view(), name='ecms-page')   # force urls to end with a slash
+    url(r'^(?P<path>.*)@admin$', CmsPageAdminRedirect.as_view(), name='ecms-admin-redirect'),
+    url(r'^$|^(?P<path>.*/)$', CmsPageView.as_view(), name='ecms-page')
 )
--- ecms/views/cmspageview.py
+++ ecms/views/cmspageview.py
@@ -1,8 +1,10 @@
 """
 The view to display CMS content.
 """
+import re
+from django.core import urlresolvers
 from ecms.models import CmsObject
-from django.views.generic import DetailView
+from django.views.generic import DetailView, RedirectView
 
 
 class CmsPageView(DetailView):
@@ -21,3 +23,17 @@ class CmsPageView(DetailView):
 
     def get_template_names(self):
         return [self.object.layout.template_path]
+
+
+class CmsPageAdminRedirect(RedirectView):
+    """
+    A view which redirects to the admin.
+    """
+    def get_redirect_url(self, **kwargs):
+        path = self.kwargs.get('path', self.request.path) or ''
+        try:
+            page = CmsObject.objects.get_for_path(path)
+            return urlresolvers.reverse('admin:ecms_cmsobject_change', args=(page.id,))
+        except CmsObject.DoesNotExist:
+            # Back to page without @admin, display the error there.
+            return '/' + re.sub('@[^@]+/?$', '', path)
