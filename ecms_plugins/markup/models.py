070dd89bb8df078afb9da4061255185190366b1b
Diederik van der Boor <vdboor@edoburu.nl>
2011-07-01 11:06:04 +0200

    markup plugin: rename formatter.py -> backend.py
    This is more generic, and a pattern of my preference to repeat.

--- ecms_plugins/markup/models.py
+++ ecms_plugins/markup/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.html import escape, linebreaks
 from ecms.extensions import CmsPageItemForm
 from ecms.models import CmsPageItem
-from ecms_plugins.markup import appsettings, formatter
+from ecms_plugins.markup import appsettings, backend
 
 
 class MarkupItemForm(CmsPageItemForm):
@@ -14,7 +14,7 @@ class MarkupItemForm(CmsPageItemForm):
     """
     def clean_text(self):
         try:
-            formatter.render_text(self.cleaned_data['text'], self.instance.language)
+            backend.render_text(self.cleaned_data['text'], self.instance.language)
         except Exception, e:
             raise ValidationError("There is an error in the markup: %s" % e)
         return self.cleaned_data['text']
@@ -28,7 +28,7 @@ class MarkupItem(CmsPageItem):
     text = models.TextField(_('markup'), blank=True)
 
     # Store the language to keep rendering intact while switching settings.
-    language = models.CharField(_('Language'), max_length=30, editable=False, default=appsettings.ECMS_MARKUP_LANGUAGE, choices=formatter.LANGUAGE_NAMES)
+    language = models.CharField(_('Language'), max_length=30, editable=False, default=appsettings.ECMS_MARKUP_LANGUAGE, choices=backend.LANGUAGE_NAMES)
 
     class Meta:
         verbose_name = _('Markup text item')
@@ -36,7 +36,7 @@ class MarkupItem(CmsPageItem):
 
     def render(self):
         try:
-            html = formatter.render_text(self.text, self.language)
+            html = backend.render_text(self.text, self.language)
         except Exception, e:
             html = '<div style="color: red; border: 1px solid red; padding: 5px;">' \
                       '<p><strong>Error:</strong></p>' \
