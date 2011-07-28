207ed6c20e4c73b4897b40ab7a8b5da326fe0073
Diederik van der Boor <vdboor@edoburu.nl>
2011-07-28 21:20:01 +0200

    ecms_dashboard: set "Back to site link" the current page.
    When a page is edited, the ReturnToSiteItem updates it's URL to match
    the current page.

--- ecms/admin/utils.py
+++ ecms/admin/utils.py
@@ -3,6 +3,7 @@ Extra utilities related to the admin
 """
 from django.core import urlresolvers
 from ecms.extensions import PLUGIN_CATEGORIES
+from ecms.models.db import CmsObject
 
 
 def get_page_admin_url(page):
@@ -10,6 +11,18 @@ def get_page_admin_url(page):
     Return the admin URL for a page.
     """
     return urlresolvers.reverse('admin:ecms_cmsobject_change', args=(page.id,))
+
+
+def get_current_edited_page(request):
+    """
+    Return the ID of the page which is currently edited.
+    Returns None if the is no page being edited.
+    """
+    match = urlresolvers.resolve(request.path_info)
+    if match.namespace == 'admin' and match.url_name == 'ecms_cmsobject_change':
+        page_id = int(match.args[0])
+        return CmsObject.objects.get(pk=page_id)
+    return None
 
 
 def get_pageitem_choices(plugins):
