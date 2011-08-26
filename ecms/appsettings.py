6f20ac0cdd4359edc73c9a9afe9d8b54f7393503
Diederik van der Boor <vdboor@edoburu.nl>
2011-08-26 19:53:03 +0200

    Add check whether the path in ECMS_TEMPLATE_DIR exists.
    This avoids much confusion when the templates can't be selected in the
    admin screens.

--- ecms/appsettings.py
+++ ecms/appsettings.py
@@ -2,6 +2,8 @@
 Overview of all settings which can be customized.
 """
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+import os
 
 ECMS_CLEAN_HTML = getattr(settings, 'ECMS_CLEAN_HTML', False)
 ECMS_SANITIZE_HTML = getattr(settings, 'ECMS_SANITIZE_HTML', False)
@@ -20,3 +22,11 @@ if ECMS_CLEAN_HTML:
 if ECMS_SANITIZE_HTML:
     from django_wysiwyg.utils import sanitize_html
     sanitize_html("<b><script>alert(1)</script></b>")
+
+# Test whether the template dir for page templates exists.
+if ECMS_TEMPLATE_DIR:
+    settingName = 'TEMPLATE_DIRS[0]' if not hasattr(settings, 'ECMS_TEMPLATE_DIR') else 'ECMS_TEMPLATE_DIR'
+    if not os.path.isabs(ECMS_TEMPLATE_DIR):
+        raise ImproperlyConfigured("The setting '{0}' needs to be an absolute path!".format(settingName))
+    if not os.path.exists(ECMS_TEMPLATE_DIR):
+        raise ImproperlyConfigured("The path '{0}' in the setting '{1}' does not exist!".format(ECMS_TEMPLATE_DIR, settingName))
