da1b1fe17a86df6c4b54171adee0d31e97fe5c34
Diederik van der Boor <vdboor@edoburu.nl>
2011-10-06 18:35:42 +0200

    Fix detection for form_designer and comments.

--- ecms_plugins/commentsarea/__init__.py
+++ ecms_plugins/commentsarea/__init__.py
@@ -7,4 +7,4 @@ backendapp = 'django.contrib.comments'
 
 # Do some settings checks.
 if backendapp not in settings.INSTALLED_APPS:
-    raise ImproperlyConfigured("The '{0}' application is required to use the '{0}' plugin.".format(backendapp, 'commentsarea'))
+    raise ImproperlyConfigured("The '{0}' application is required to use the '{1}' plugin.".format(backendapp, 'commentsarea'))
