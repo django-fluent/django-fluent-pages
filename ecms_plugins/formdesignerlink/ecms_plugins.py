98ec9896c8c99e19f43412890387e3e34250c900
Diederik van der Boor <vdboor@edoburu.nl>
2011-10-11 18:43:22 +0200

    formdesignerlink: add docs for more important settings.

--- ecms_plugins/formdesignerlink/ecms_plugins.py
+++ ecms_plugins/formdesignerlink/ecms_plugins.py
@@ -2,6 +2,12 @@
 Form designer link plugin.
 
 This plugin displays a form at the page, that was created with form_designer.
+To customize the output, configure the ``django-form-designer`` application via the settings file.
+For example, use:
+
+ * ``FORM_DESIGNER_DEFAULT_FORM_TEMPLATE`` or ``FORM_TEMPLATES`` to control the form output (e.g. render it with ``django-uni-form``).
+ * ``FORM_DESIGNER_FIELD_CLASSES`` to define which field types are allowed.
+ * ``FORM_DESIGNER_WIDGET_CLASSES`` to define which widgets are allowed.
 """
 from __future__ import absolute_import
 from form_designer import settings as form_designer_settings
