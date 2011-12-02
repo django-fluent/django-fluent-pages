aa6691fb5f2a2ac9412c3086ad8abd08b2a3dc9a
Diederik van der Boor <vdboor@edoburu.nl>
2011-12-02 17:53:14 +0100

    Added Google Docs Viewer plugin (useful for proof of concept demo's)
    Also moved _validate_size => ecms.utils.validators.validate_html_size()

--- ecms_plugins/googledocsviewer/models.py
+++ ecms_plugins/googledocsviewer/models.py
@@ -4,17 +4,17 @@ from ecms.models import CmsPageItem
 from ecms.utils.validators import validate_html_size
 
 
-class IframeItem(CmsPageItem):
+class GoogleDocsViewerItem(CmsPageItem):
     """
-    An ``<iframe>`` that is displayed at the page..
+    A Google Docs viewer that is displayed at the page.
     """
-    src = models.URLField(_("Page URL"))
+    url = models.URLField(_("File URL"), help_text=_("Specify the URL of an online document, for example a PDF or DOCX file."))
     width = models.CharField(_("Width"), max_length=10, validators=[validate_html_size], default="100%", help_text=_("Specify the size in pixels, or a percentage of the container area size."))
     height = models.CharField(_("Height"), max_length=10, validators=[validate_html_size], default="600", help_text=_("Specify the size in pixels."))
 
     class Meta:
-        verbose_name = _("Iframe")
-        verbose_name_plural = _("Iframes")
+        verbose_name = _("Google Docs viewer")
+        verbose_name_plural = _("Google Docs viewers")
 
     def __unicode__(self):
-        return self.src
+        return self.url
