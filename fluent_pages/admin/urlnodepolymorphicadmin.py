2f4b9bb3210715ff0086887179af6c3e1994a174
Diederik van der Boor <vdboor@edoburu.nl>
2012-01-14 18:57:42 +0100

    Replace manual <link> tag with class Media

--- fluent_pages/admin/urlnodepolymorphicadmin.py
+++ fluent_pages/admin/urlnodepolymorphicadmin.py
@@ -27,6 +27,12 @@ class UrlNodePolymorphicAdmin(PolymorphicBaseModelAdmin, MPTTModelAdmin):
     change_list_template = None  # Restore Django's default search behavior, no admin/mptt_change_list.html
 
 
+    class Media:
+        css = {
+            'screen': ('fluent_pages/admin.css',)
+        }
+
+
     # ---- Polymorphic code ----
 
     def get_admin_for_model(self, model):
