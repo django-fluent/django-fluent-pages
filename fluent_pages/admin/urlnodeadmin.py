4cdf552d55919bf4daa4cc1528892c46dac3abba
Diederik van der Boor <vdboor@edoburu.nl>
2012-01-14 18:55:08 +0100

    Move UrlNodeAdmin list code to UrlNodePolymorphicAdmin
    This is after all, the only place where it is really being used.
    Also removes the need to subclass UrlNodeAdmin, as it has conflicting
    uses (it is now solely a base class for derived pagetypes).

--- fluent_pages/admin/urlnodeadmin.py
+++ fluent_pages/admin/urlnodeadmin.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from mptt.admin import MPTTModelAdmin
@@ -83,14 +82,6 @@ class UrlNodeAdmin(PolymorphedModelAdmin, MPTTModelAdmin):
     base_form = UrlNodeAdminForm
 
 
-    # Config list page:
-    list_display = ('title', 'status_column', 'modification_date', 'actions_column')
-    #list_filter = ('status', 'parent')
-    search_fields = ('slug', 'title')
-    actions = ['make_published']
-    change_list_template = None  # Restore Django's default search behavior, no admin/mptt_change_list.html
-
-
     # Expose fieldsets for subclasses to reuse
     FIELDSET_GENERAL = (None, {
         'fields': ('title', 'slug', 'status',),
@@ -104,15 +95,15 @@ class UrlNodeAdmin(PolymorphedModelAdmin, MPTTModelAdmin):
         'classes': ('collapse',),
     })
 
-    # Config add/edit:
-    prepopulated_fields = { 'slug': ('title',), }
-    raw_id_fields = ['parent']
     base_fieldsets = (
         FIELDSET_GENERAL,
         FIELDSET_MENU,
         FIELDSET_PUBLICATION
     )
 
+    # Config add/edit page:
+    prepopulated_fields = { 'slug': ('title',), }
+    raw_id_fields = ['parent']
     radio_fields = {'status': admin.HORIZONTAL}
 
 
@@ -165,60 +156,6 @@ class UrlNodeAdmin(PolymorphedModelAdmin, MPTTModelAdmin):
             for field in fieldset[1]['fields']:
                 subclass_fields.remove(field)
         return subclass_fields
-
-
-    # ---- list actions ----
-
-    STATUS_ICONS = (
-        (UrlNode.PUBLISHED, 'img/admin/icon-yes.gif'),
-        (UrlNode.DRAFT,     'img/admin/icon-unknown.gif'),
-    )
-
-    def status_column(self, urlnode):
-        status = urlnode.status
-        title = [rec[1] for rec in UrlNode.STATUSES if rec[0] == status].pop()
-        icon  = [rec[1] for rec in self.STATUS_ICONS  if rec[0] == status].pop()
-        return u'<img src="{admin}{icon}" width="10" height="10" alt="{title}" title="{title}" />'.format(
-            admin=settings.ADMIN_MEDIA_PREFIX, icon=icon, title=title)
-
-    status_column.allow_tags = True
-    status_column.short_description = _('Status')
-
-
-    def actions_column(self, urlnode):
-        return u' '.join(self._actions_column_icons(urlnode))
-
-    actions_column.allow_tags = True
-    actions_column.short_description = _('actions')
-
-
-    def _actions_column_icons(self, urlnode):
-        actions = [
-            u'<a href="add/?{parentattr}={id}" title="{title}"><img src="{static}fluent_pages/img/admin/page_new.gif" width="16" height="16" alt="{title}" /></a>'.format(
-                parentattr=self.model._mptt_meta.parent_attr, id=urlnode.pk, title=_('Add child'), static=settings.STATIC_URL)
-        ]
-
-        if hasattr(urlnode, 'get_absolute_url') and urlnode.is_published:
-            actions.append(
-                u'<a href="{url}" title="{title}" target="_blank"><img src="{static}fluent_pages/img/admin/world.gif" width="16" height="16" alt="{title}" /></a>'.format(
-                    url=urlnode.get_absolute_url(), title=_('View on site'), static=settings.STATIC_URL)
-                )
-        return actions
-
-
-    def make_published(self, request, queryset):
-        rows_updated = queryset.update(status=UrlNode.PUBLISHED)
-
-        if rows_updated == 1:
-            message = "1 page was marked as published."
-        else:
-            message = "{0} pages were marked as published.".format(rows_updated)
-        self.message_user(request, message)
-
-
-    make_published.short_description = _("Mark selected objects as published")
-
-
 
 
     # ---- Pass parent_object to templates ----
--- fluent_pages/admin/urlnodepolymorphicadmin.py
+++ fluent_pages/admin/urlnodepolymorphicadmin.py
@@ -19,6 +19,15 @@ class UrlNodePolymorphicAdmin(PolymorphicBaseModelAdmin, MPTTModelAdmin):
     base_model = UrlNode
     add_type_form = PageTypeChoiceAdminForm
 
+    # Config list page:
+    list_display = ('title', 'status_column', 'modification_date', 'actions_column')
+    #list_filter = ('status', 'parent')
+    search_fields = ('slug', 'title')
+    actions = ['make_published']
+    change_list_template = None  # Restore Django's default search behavior, no admin/mptt_change_list.html
+
+
+    # ---- Polymorphic code ----
 
     def get_admin_for_model(self, model):
         from fluent_pages.extensions import page_type_pool
@@ -41,3 +50,56 @@ class UrlNodePolymorphicAdmin(PolymorphicBaseModelAdmin, MPTTModelAdmin):
             ct = ContentType.objects.get_for_model(plugin.model)
             choices.append((ct.id, plugin.verbose_name))
         return choices
+
+
+    # ---- List code ----
+
+    STATUS_ICONS = (
+        (UrlNode.PUBLISHED, 'img/admin/icon-yes.gif'),
+        (UrlNode.DRAFT,     'img/admin/icon-unknown.gif'),
+    )
+
+
+    def status_column(self, urlnode):
+        status = urlnode.status
+        title = [rec[1] for rec in UrlNode.STATUSES if rec[0] == status].pop()
+        icon  = [rec[1] for rec in self.STATUS_ICONS  if rec[0] == status].pop()
+        return u'<img src="{admin}{icon}" width="10" height="10" alt="{title}" title="{title}" />'.format(
+            admin=settings.ADMIN_MEDIA_PREFIX, icon=icon, title=title)
+
+    status_column.allow_tags = True
+    status_column.short_description = _('Status')
+
+
+    def actions_column(self, urlnode):
+        return u' '.join(self._actions_column_icons(urlnode))
+
+    actions_column.allow_tags = True
+    actions_column.short_description = _('actions')
+
+
+    def _actions_column_icons(self, urlnode):
+        actions = [
+            u'<a href="add/?{parentattr}={id}" title="{title}"><img src="{static}fluent_pages/img/admin/page_new.gif" width="16" height="16" alt="{title}" /></a>'.format(
+                parentattr=self.model._mptt_meta.parent_attr, id=urlnode.pk, title=_('Add child'), static=settings.STATIC_URL)
+        ]
+
+        if hasattr(urlnode, 'get_absolute_url') and urlnode.is_published:
+            actions.append(
+                u'<a href="{url}" title="{title}" target="_blank"><img src="{static}fluent_pages/img/admin/world.gif" width="16" height="16" alt="{title}" /></a>'.format(
+                    url=urlnode.get_absolute_url(), title=_('View on site'), static=settings.STATIC_URL)
+                )
+        return actions
+
+
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status=UrlNode.PUBLISHED)
+
+        if rows_updated == 1:
+            message = "1 page was marked as published."
+        else:
+            message = "{0} pages were marked as published.".format(rows_updated)
+        self.message_user(request, message)
+
+
+    make_published.short_description = _("Mark selected objects as published")
