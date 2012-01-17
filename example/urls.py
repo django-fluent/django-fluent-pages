b32157aabfd39e0242bffe2c481f5c7607262476
Diederik van der Boor <vdboor@edoburu.nl>
2012-01-17 09:33:24 +0100

    Added simple example app

---
+++ example/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include('fluent_pages.urls')),
+)
