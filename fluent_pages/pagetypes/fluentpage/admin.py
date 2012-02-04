599fdc13b12d388bba7c8a4bf1ecef53a986f518
Diederik van der Boor <vdboor@edoburu.nl>
2012-02-04 21:27:33 +0100

    Removed fieldset title style, all handled by django-fluent-contents now.
    Improves display and removes layout differences between both modules.

--- fluent_pages/pagetypes/fluentpage/admin.py
+++ fluent_pages/pagetypes/fluentpage/admin.py
@@ -27,9 +27,6 @@ class FluentPageAdmin(PlaceholderEditorAdmin, PageAdmin):
 
     class Media:
         js = ('fluent_pages/fluentpage/fluent_layouts.js',)
-        css = {
-            'screen': ('fluent_pages/fluentpage/admin.css',),
-        }
 
 
     # ---- fluent-contents integration ----
