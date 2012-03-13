5e2147caeb10c29d26891586737f8ee42ae3f7bc
Diederik van der Boor <vdboor@edoburu.nl>
2012-03-13 09:09:54 +0100

    Add ignore_multiple argument to mixed_reverse / app_reverse

--- fluent_pages/urlresolvers.py
+++ fluent_pages/urlresolvers.py
@@ -12,17 +12,17 @@ class PageTypeNotMounted(NoReverseMatch):
     pass
 
 
-def mixed_reverse(viewname, args=None, kwargs=None, current_app=None, current_page=None, multiple=False):
+def mixed_reverse(viewname, args=None, kwargs=None, current_app=None, current_page=None, multiple=False, ignore_multiple=False):
     """
     Attempt to reverse a normal URLconf URL, revert to app_reverse() on errors.
     """
     try:
         return reverse(viewname, args=args, kwargs=kwargs, current_app=current_app)
     except NoReverseMatch:
-        return app_reverse(viewname, args=args, kwargs=kwargs, multiple=multiple, current_page=current_page)
+        return app_reverse(viewname, args=args, kwargs=kwargs, multiple=multiple, ignore_multiple=ignore_multiple, current_page=current_page)
 
 
-def app_reverse(viewname, args=None, kwargs=None, multiple=False, current_page=None):
+def app_reverse(viewname, args=None, kwargs=None, multiple=False, ignore_multiple=False, current_page=None):
     """
     Locate an URL which is located under a page type.
     """
@@ -35,7 +35,7 @@ def app_reverse(viewname, args=None, kwargs=None, multiple=False, current_page=N
     plugin, url_end = _find_plugin_reverse(viewname, args, kwargs)
     pages = _get_pages_of_type(plugin.model)
 
-    if len(pages) > 1 and not multiple:
+    if len(pages) > 1 and not (multiple or ignore_multiple):
         # Multiple results available.
         # If there is a current page, it can be used as base URL, otherwise bail out.
         if current_page and current_page.plugin is plugin:
@@ -53,6 +53,7 @@ def app_reverse(viewname, args=None, kwargs=None, multiple=False, current_page=N
     if multiple:
         return (page.get_absolute_url() + url_end for page in pages)
     else:
+        # single result, or ignoring multiple results.
         return pages[0].get_absolute_url() + url_end
 
 
