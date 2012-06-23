2beb3a51188845c1499b26e7f55376366f36fd95
Diederik van der Boor <vdboor@edoburu.nl>
2012-06-23 15:07:23 +0200

    Add repr for PageTypePlugin

--- fluent_pages/extensions.py
+++ fluent_pages/extensions.py
@@ -67,6 +67,10 @@ class PageTypePlugin(object):
         self._url_resolver = None
 
 
+    def __repr__(self):
+        return '<{0} for {1} model>'.format(self.__class__.__name__, unicode(self.model.__name__).encode('ascii'))
+
+
     @property
     def verbose_name(self):
         """
