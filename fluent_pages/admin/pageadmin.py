c08881514d3caeb5407f9fe9e695f5dbf74d8461
Diederik van der Boor <vdboor@edoburu.nl>
2012-07-20 19:17:07 +0200

    Fix displaying the 'parent' field as text field
    Django 1.4 rightfully detects that the proxied UrlNode class is not
    registered in the admin. Override the relation to fix the display.

--- fluent_pages/admin/pageadmin.py
+++ fluent_pages/admin/pageadmin.py
@@ -1,3 +1,5 @@
+import copy
+from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 from django.utils.translation import ugettext_lazy as _
 from fluent_pages.admin.urlnodeadmin import UrlNodeAdmin, UrlNodeAdminForm
 from fluent_pages.models import Page, HtmlPage
@@ -17,9 +19,22 @@ class PageAdmin(UrlNodeAdmin):
     class Media:
         js = ('fluent_pages/fk_raw_id_fix.js',)
 
-    # This class may be extended in the future.
-    # Currently it's a tagging placeholder
-    # to have a clear Page <-> PageAdmin combo.
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        field = super(PageAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        if field is None:
+            return None
+
+        # Hack the ForeignKeyRawIdWidget in Django 1.4 to display a selector for the base model too.
+        # It correctly detects that the parent field actually points to an UrlNode, instead of Page.
+        # Since UrlNode is not registered in the admin, it won't display the selector. Overriding that here.
+        # It also partially fixes Django 1.3, which would wrongly point the url to ../../../fluent_pages/urlnode/ otherwise.
+        if db_field.name == 'parent' and isinstance(field.widget, ForeignKeyRawIdWidget):
+            field.widget.rel = copy.copy(field.widget.rel)
+            field.widget.rel.to = Page
+
+        return field
+
 
 
 class HtmlPageAdmin(PageAdmin):
