0383f300d0be1c9bfe564c4fc35810aac721c3b9
Diederik van der Boor <vdboor@edoburu.nl>
2012-07-22 22:20:12 +0200

    Revering pull #3 because it breaks the original reason for splitting
    The layout mechanism is tied to the fluentpage app, in case you like to
    use a different layout mechanism with *django-fluent-pages* involved,
    it's recommended to create a new pagetype app. See remaining reasoning
    in the docstring.
    
    Reverts: 2a3b20a90528c8e3aa56df6be5c8fa1941146222

--- fluent_pages/pagetypes/fluentpage/models.py
+++ fluent_pages/pagetypes/fluentpage/models.py
@@ -6,8 +6,17 @@ from fluent_pages.models import HtmlPage, PageLayout
 class FluentPageBase(HtmlPage):
     """
     A ```FluentPage``` represents one HTML page of the site.
+
+    This class is abstract, so it's easy to reuse the same CMS functionality in your custom page types
+    without introducing another table/join indirection in the database. Naturally, the same layout mechanism is used.
+    In case the ``layout`` should be handled differently, please consider building a variation of this page type application.
+
+    The API to interface with *django-fluent-contents* is public, and documented for this reason.
+    In fact, this application is just a tiny bridge between the page type plugins and the *django-fluent-pages* API.
     """
-    
+    # Allow NULL in the layout, so this system can still be made optional in the future in favor of a configuration setting.
+    layout = models.ForeignKey(PageLayout, verbose_name=_('Layout'), null=True)
+
     # Access to fluent-contents via the model
     placeholder_set = PlaceholderRelation()
     contentitem_set = ContentItemRelation()
@@ -25,4 +34,4 @@ class FluentPage(FluentPageBase):
     """
     A ```FluentPage``` represents one HTML page of the site.
     """
-    layout = models.ForeignKey(PageLayout, verbose_name=_('Layout'))
+    pass
