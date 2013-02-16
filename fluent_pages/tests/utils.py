ecaafab5554bc1752a9e3ba3d6311793835eb32f
Diederik van der Boor <vdboor@edoburu.nl>
2013-02-16 13:52:48 +0100

    Code cleanup in tests/utils.py

--- fluent_pages/tests/utils.py
+++ fluent_pages/tests/utils.py
@@ -6,9 +6,9 @@ from django.contrib.sites.models import Site
 from django.db.models import loading
 from django.template.loaders import app_directories
 from django.test import TestCase
+from django.utils.importlib import import_module
 from fluent_pages.models.db import UrlNode
 from fluent_pages.utils.compat import get_user_model
-from fluent_pages.tests import testapp
 import os
 
 
@@ -17,24 +17,33 @@ class AppTestCase(TestCase):
     Tests for URL resolving.
     """
     user = None
+    install_apps = (
+        'fluent_pages.tests.testapp',
+    )
 
 
     @classmethod
     def setUpClass(cls):
         User = get_user_model()
 
-        if 'fluent_pages.tests.testapp' not in settings.INSTALLED_APPS:
+        if cls.install_apps:
             # When running this app via `./manage.py test fluent_pages`, auto install the test app + models.
-            print 'Adding fluent_pages.tests.testapp to INSTALLED_APPS'
-            settings.INSTALLED_APPS += ('fluent_pages.tests.testapp',)
+            run_syncdb = False
+            for appname in cls.install_apps:
+                if appname not in settings.INSTALLED_APPS:
+                    print 'Adding {0} to INSTALLED_APPS'.format(appname)
+                    settings.INSTALLED_APPS += (appname,)
+                    run_syncdb = True
 
-            # Flush caches
-            loading.cache.loaded = False
-            app_directories.app_template_dirs += (
-                os.path.join(os.path.dirname(testapp.__file__), 'templates'),
-            )
+                    # Flush caches
+                    testapp = import_module(appname)
+                    loading.cache.loaded = False
+                    app_directories.app_template_dirs += (
+                        os.path.join(os.path.dirname(testapp.__file__), 'templates'),
+                    )
 
-            call_command('syncdb', verbosity=0)  # may run south's overlaid version
+            if run_syncdb:
+                call_command('syncdb', verbosity=0)  # may run south's overlaid version
 
         # Create basic objects
         # 1.4 does not create site automatically with the defined SITE_ID, 1.3 does.
