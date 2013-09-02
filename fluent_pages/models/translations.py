b561b8e0a69ecdc1ac640992326f19e99e081db6
Diederik van der Boor <vdboor@edoburu.nl>
2013-09-02 12:50:03 +0200

    Make sure all other translations are saved on `.save()`

--- fluent_pages/models/translations.py
+++ fluent_pages/models/translations.py
@@ -51,6 +51,11 @@ class TranslatableModel(models.Model):
             for field, value in translated_kwargs.iteritems():
                 setattr(translation, field, value)
 
+            # Check if other translations were also modified
+            for code, obj in self._translations_cache.iteritems():
+                if code != translation.language_code and obj.is_modified:
+                    obj.save()
+
 
     def get_current_language(self):
         # not a property, so won't conflict with model fields.
