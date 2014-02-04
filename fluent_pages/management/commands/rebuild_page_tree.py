78200a9aff3f3135fc5c0fac84e17a3dd0112fc8
Diederik van der Boor <vdboor@edoburu.nl>
2014-02-04 03:37:13 +0100

    updated rebuild_page_tree (still incomplete/untested)

--- fluent_pages/management/commands/rebuild_page_tree.py
+++ fluent_pages/management/commands/rebuild_page_tree.py
@@ -1,5 +1,6 @@
 from django.core.management.base import NoArgsCommand
 from django.utils.encoding import smart_text
+from fluent_pages import appsettings
 from fluent_pages.models.db import UrlNode_Translation, UrlNode
 
 
@@ -18,12 +19,21 @@ class Command(NoArgsCommand):
             slugs.setdefault(translation.language_code, {})[translation.master_id] = translation.slug
             overrides.setdefault(translation.language_code, {})[translation.master_id] = translation.override_url
 
-            translation._cached_url = self._construct_url(translation.language_code, translation.master_id, parents, slugs, overrides)
-            translation.save()
-            self.stdout.write(smart_text(u"- {0}\t {1} {2}\n".format(translation.master_id, translation.language_code, translation._cached_url)))
+            old_url = translation._cached_url
+            new_url = self._construct_url(translation.language_code, translation.master_id, parents, slugs, overrides)
+            if old_url != new_url:
+                translation._cached_url = new_url
+                translation.save()
+
+            if old_url != new_url:
+                self.stdout.write(smart_text(u"- {0}\t {1} {2} UPDATED from {3}\n".format(translation.master_id, translation.language_code, translation._cached_url, old_url)))
+            else:
+                self.stdout.write(smart_text(u"- {0}\t {1} {2}\n".format(translation.master_id, translation.language_code, translation._cached_url)))
 
 
     def _construct_url(self, language_code, child_id, parents, slugs, overrides):
+        fallback = appsettings.FLUENT_PAGES_LANGUAGES.get_fallback_language(language_code)
+
         breadcrumb = []
         cur = child_id
         while cur is not None:
@@ -40,6 +50,9 @@ class Command(NoArgsCommand):
                     continue
             except KeyError:
                 pass
-            url_parts.append(slugs[language_code][id])
+            try:
+                url_parts.append(slugs[language_code][id])
+            except KeyError:
+                url_parts.append(slugs[fallback][id])
 
         return (u'/'.join(url_parts) + u'/').replace('//', '/')
