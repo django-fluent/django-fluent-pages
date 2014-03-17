707e0c22897d917994bb2c0b7db7698c3505433f
Diederik van der Boor <vdboor@edoburu.nl>
2014-03-17 10:07:17 +0100

    fix UrlNodeAdminForm, allow override_url to be missing

--- fluent_pages/admin/urlnodechildadmin.py
+++ fluent_pages/admin/urlnodechildadmin.py
@@ -32,7 +32,8 @@ class UrlNodeAdminForm(MPTTAdminForm, TranslatableModelForm):
         else:
             super(UrlNodeAdminForm, self).__init__(*args, **kwargs)
 
-        self.fields['override_url'].language_code = self.language_code
+        if 'override_url' in self.fields:
+            self.fields['override_url'].language_code = self.language_code
 
     def clean(self):
         """
