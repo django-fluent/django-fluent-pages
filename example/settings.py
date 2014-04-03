b71ad14664a7b214c9dd4191714925bb7234e53c
Diederik van der Boor <vdboor@edoburu.nl>
2014-04-03 12:07:36 +0200

    example settings: remove LOGGING

--- example/settings.py
+++ example/settings.py
@@ -120,22 +120,3 @@ INSTALLED_APPS = (
 
 #DJANGO_WYSIWYG_FLAVOR = 'yui_advanced'
 DJANGO_WYSIWYG_FLAVOR = 'tinymce_advanced'
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
