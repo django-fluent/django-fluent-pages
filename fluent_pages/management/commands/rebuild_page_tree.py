7df67341b32af7eb2dbb1eb165861cc1af7e4424
Diederik van der Boor <vdboor@edoburu.nl>
2014-05-07 11:41:27 +0200

    Improve rebuild_page_tree command.
    - add `--dry-run` option.
    - call TreeManager.rebuild()
    - add proper Python column formatting

--- fluent_pages/management/commands/rebuild_page_tree.py
+++ fluent_pages/management/commands/rebuild_page_tree.py
@@ -1,5 +1,6 @@
 from django.core.management.base import NoArgsCommand
 from django.utils.encoding import smart_text
+from optparse import make_option
 from fluent_pages import appsettings
 from fluent_pages.models.db import UrlNode_Translation, UrlNode
 
@@ -9,13 +10,31 @@ class Command(NoArgsCommand):
     Update the tree, rebuild the translated URL nodes.
     """
     help = "Update the cached_url for the translated URL node tree"
+    option_list = (
+        make_option('-p', '--dry-run', action='store_true', dest='dry-run', default=False,
+            help="Only list what will change, don't make the actual changes"),
+    ) + NoArgsCommand.option_list
 
     def handle_noargs(self, **options):
+        is_dry_run = options.get('dry-run', False)
         slugs = {}
         overrides = {}
         parents = dict(UrlNode.objects.values_list('id', 'parent_id'))
 
-        for translation in UrlNode_Translation.objects.order_by('master__lft', 'master__tree_id'):
+        if not is_dry_run:
+            self.stdout.write("Updated MPTT columns")
+            UrlNode.objects.rebuild()
+            self.stdout.write("Updating cached URLs")
+            self.stdout.write("Page tree nodes:\n\n")
+
+        col_style = "| {0:6} | {1:6} | {2:6} | {3}"
+        header = col_style.format("Site", "Page", "Locale", "URL")
+        sep = '-' * (len(header) + 40)
+        self.stdout.write(sep)
+        self.stdout.write(header)
+        self.stdout.write(sep)
+
+        for translation in UrlNode_Translation.objects.select_related('master').order_by('master__parent_site__id', 'master__tree_id', 'master__lft', 'language_code'):
             slugs.setdefault(translation.language_code, {})[translation.master_id] = translation.slug
             overrides.setdefault(translation.language_code, {})[translation.master_id] = translation.override_url
 
@@ -23,12 +42,19 @@ class Command(NoArgsCommand):
             new_url = self._construct_url(translation.language_code, translation.master_id, parents, slugs, overrides)
             if old_url != new_url:
                 translation._cached_url = new_url
-                translation.save()
+                if not is_dry_run:
+                    translation.save()
 
             if old_url != new_url:
-                self.stdout.write(smart_text(u"- {0}\t {1} {2} UPDATED from {3}\n".format(translation.master_id, translation.language_code, translation._cached_url, old_url)))
+                self.stdout.write(smart_text(u"{0}  {1} {2}\n".format(
+                    col_style.format(translation.master.parent_site_id, translation.master_id, translation.language_code, translation._cached_url),
+                    "WILL CHANGE from" if is_dry_run else "UPDATED from",
+                    old_url
+                )))
             else:
-                self.stdout.write(smart_text(u"- {0}\t {1} {2}\n".format(translation.master_id, translation.language_code, translation._cached_url)))
+                self.stdout.write(smart_text(col_style.format(
+                    translation.master.parent_site_id, translation.master_id, translation.language_code, translation._cached_url
+                )))
 
 
     def _construct_url(self, language_code, child_id, parents, slugs, overrides):
