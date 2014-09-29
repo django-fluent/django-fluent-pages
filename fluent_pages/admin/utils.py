f3b82743363584314ead03a9f21f9314c4065327
Diederik van der Boor <vdboor@edoburu.nl>
2014-09-29 20:52:00 +0200

    Provide backwards compatibility for fluent_pages.admin.utils import

---
+++ fluent_pages/admin/utils.py
@@ -0,0 +1,7 @@
+import warnings
+from fluent_pages.adminui.utils import get_page_admin_url, get_current_edited_page
+
+warnings.warn(DeprecationWarning(
+    "Please use `fluent_pages.adminui.utils` instead, "
+    "the `fluent_pages.admin.utils` module is deprecated for Django 1.7 compatibility."
+))
