7146522af16a58258eb3a023e8389b6aca3f602d
Stuart Dines <stuart@interaction.net.au>
2015-05-15 08:17:20 +1000

    Add AppConfig.

--- fluent_pages/__init__.py
+++ fluent_pages/__init__.py
@@ -1,2 +1,3 @@
 # following PEP 440
 __version__ = "0.9"
+default_app_config = 'fluent_pages.apps.FluentPagesConfig'
---
+++ fluent_pages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FluentPagesConfig(AppConfig):
+    name = 'fluent_pages'
+    verbose_name = 'Fluent Pages'
