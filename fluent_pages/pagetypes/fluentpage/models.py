7fb3c00aff33192f07474bb2711cb018702be917
Basil Shubin <basil.shubin@gmail.com>
2015-10-07 14:30:11 +0600

    allow custom layout per language

--- fluent_pages/pagetypes/fluentpage/models.py
+++ fluent_pages/pagetypes/fluentpage/models.py
@@ -3,11 +3,14 @@ from django.utils.translation import ugettext_lazy as _
 from fluent_pages.models import PageLayout
 from fluent_pages.integration.fluent_contents.models import FluentContentsPage
 
+from parler.fields import TranslatedField
+from parler.models import TranslatableModel, TranslatedFieldsModel
+
 
 # This all exists for backwards compatibility
 # The new v0.9 method is using fluent_pages.integration.fluent_contents, instead of directly inheriting this app.
 # In that way, the pagetypes are stand-alone apps again.
-class AbstractFluentPage(FluentContentsPage):
+class AbstractFluentPage(FluentContentsPage, TranslatableModel):
     """
     A ```FluentPage``` represents one HTML page of the site.
 
@@ -20,19 +23,27 @@ class AbstractFluentPage(FluentContentsPage):
     without introducing another table/join indirection in the database. Naturally, the same layout mechanism is used.
     In case the ``layout`` should be handled differently, please consider building a variation of this page type application.
     """
-    # Allow NULL in the layout, so this system can still be made optional in the future in favor of a configuration setting.
-    layout = models.ForeignKey(PageLayout, verbose_name=_('Layout'), null=True)
+    layout = TranslatedField(any_language=True)
 
     class Meta:
         abstract = True
         verbose_name = _("Page")
         verbose_name_plural = _("Pages")
+
+
+class AbstractFluentPageTranslation(TranslatedFieldsModel):
+
+    # Allow NULL in the layout, so this system can still be made
+    # optional in the future in favor of a configuration setting.
+    layout = models.ForeignKey(PageLayout, verbose_name=_('Layout'), null=True)
+
+    master = None
+
+    class Meta:
+        abstract = True
         permissions = (
             ('change_page_layout', _("Can change Page layout")),
         )
-
-# For even more backwards compatibility
-FluentPageBase = AbstractFluentPage
 
 
 class FluentPage(AbstractFluentPage):
@@ -40,3 +51,8 @@ class FluentPage(AbstractFluentPage):
     A ```FluentPage``` represents one HTML page of the site.
     """
     pass
+
+
+class FluentPageTranslation(AbstractFluentPageTranslation):
+
+    master = models.ForeignKey(FluentPage, related_name='page_translations')
