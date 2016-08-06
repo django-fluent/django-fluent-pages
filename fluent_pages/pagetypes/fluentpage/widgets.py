0a6c1fd48a76bf000839c1c4142b54505449bb08
Diederik van der Boor <vdboor@edoburu.nl>
2016-08-06 18:19:14 +0200

    Fix Django 1.10 rendering of `LayoutSelector` widget in admin edit

--- fluent_pages/pagetypes/fluentpage/widgets.py
+++ fluent_pages/pagetypes/fluentpage/widgets.py
@@ -1,8 +1,9 @@
+import django
 from django.forms.widgets import Select
 
 
 class LayoutSelector(Select):
 
-    def render(self, name, value, attrs=None, choices=()):
+    def render(self, name, value, attrs=None, choices=()):  # Django 1.10: choices is no longer used.
         attrs['data-original-value'] = value
-        return super(LayoutSelector, self).render(name, value, attrs, choices)
+        return super(LayoutSelector, self).render(name, value, attrs)
