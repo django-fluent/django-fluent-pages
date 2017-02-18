a853db44ccd4c40e44a9d686c31b6d547da8a939
Diederik van der Boor <vdboor@edoburu.nl>
2017-02-18 22:34:50 +0100

    Fix the example app

--- example/settings.py
+++ example/settings.py
@@ -7,7 +7,6 @@ import sys
 sys.path.insert(0, dirname(dirname(realpath(__file__))))
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -45,12 +44,30 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '-#@bi6bue%#1j)6+4b&#i0g-*xro@%f@_#zwv=2-g_@n3n_kj5'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(dirname(__file__), "templates"),
+        ],
+        'OPTIONS': {
+            'debug': DEBUG,
+            'loaders': (
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ),
+            'context_processors': (
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ),
+        },
+    },
+]
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -60,20 +77,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.request',
-    'django.core.context_processors.static',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-)
-
 ROOT_URLCONF = 'urls'
-
-TEMPLATE_DIRS = (
-    join(dirname(__file__), "templates"),
-)
 
 FIXTURE_DIRS = (
     join(dirname(__file__), "fixtures"),
--- example/urls.py
+++ example/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+import fluent_pages.urls
+import tinymce.urls
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/utils/tinymce/', include('tinymce.urls')),
+    url(r'^admin/utils/tinymce/', include(tinymce.urls)),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'', include('fluent_pages.urls')),
+    url(r'', include(fluent_pages.urls)),
 ]
