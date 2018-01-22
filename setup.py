cfe970a52a7bf800e82ea153fb1a24a7258db322
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-22 17:25:01 +0100

    fix setup classifiers

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'Pillow',  # needed by ImageField
     ],
     requires=[
-        'Django (>=1.7)',
+        'Django (>=1.10)',
     ],
     extras_require={
         'tests': [
@@ -86,12 +86,13 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django',
-        'Framework :: Django :: 1.7',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
