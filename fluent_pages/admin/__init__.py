02c8f22e09d1aea8f0870936e21a7006347346d1
Diederik van der Boor <vdboor@edoburu.nl>
2019-07-11 20:33:49 +0200

    Delay page type plugin.admin_class registration, which fixes admin registration errors

--- fluent_pages/admin/__init__.py
+++ fluent_pages/admin/__init__.py
@@ -4,9 +4,7 @@ The admin site registration.
 This is separate from the admin classes, so those can be imported freely without invoking ``admin.site.register()``.
 The admin site registration is only possible in Django 1.7 once all models are loaded.
 """
-import re
 from django.contrib import admin
-from importlib import import_module
 
 from fluent_pages.models import Page, PageLayout
 
@@ -21,38 +19,6 @@ from fluent_pages.adminui import (
     PageAdminForm,
 )
 
-
-# -------- Model registration --------
-
-def _register_subclass_types():
-    """
-    See if any page type plugin uses the old registration system, and register that
-    Previously django-polymorphic registered all subclasses internally.
-    Nowadays, all subclasses should just be registered in the regular admin.
-    """
-    RE_PLUGIN_MODULE = re.compile(r'\.page_type_plugins\b.*$')
-
-    from fluent_pages.extensions import page_type_pool
-    for plugin in page_type_pool.get_plugins():
-        if plugin.model in admin.site._registry:
-            continue
-
-        # First try to perform an admin file import, it may register itself.
-        admin_path = RE_PLUGIN_MODULE.sub('.admin', plugin.__module__)
-        try:
-            import_module(admin_path)
-        except ImportError:
-            pass
-        else:
-            if plugin.model in admin.site._registry:
-                continue
-
-        # Register the admin, since the plugin didn't do this.
-        if getattr(plugin, 'model_admin', None):
-            admin.site.register(plugin.model, plugin.model_admin)
-
-
 # Register the models with the admin site
-_register_subclass_types()
 admin.site.register(Page, admin_class=PageParentAdmin)
 admin.site.register(PageLayout, admin_class=PageLayoutAdmin)
--- fluent_pages/apps.py
+++ fluent_pages/apps.py
@@ -1,6 +1,38 @@
+import re
+
 from django.apps import AppConfig
+
+from fluent_utils.load import import_module_or_none
 
 
 class FluentPagesConfig(AppConfig):
     name = 'fluent_pages'
     verbose_name = 'Fluent Pages'
+
+    def ready(self):
+        _register_subclass_types()
+
+
+def _register_subclass_types():
+    """
+    See if any page type plugin uses the old registration system, and register that
+    Previously django-polymorphic registered all subclasses internally.
+    Nowadays, all subclasses should just be registered in the regular admin.
+    """
+    from django.contrib import admin
+    RE_PLUGIN_MODULE = re.compile(r'\.page_type_plugins\b.*$')
+
+    from fluent_pages.extensions import page_type_pool
+    for plugin in page_type_pool.get_plugins():
+        if plugin.model in admin.site._registry:
+            continue
+
+        # First try to perform an admin file import, it may register itself.
+        admin_path = RE_PLUGIN_MODULE.sub('.admin', plugin.__module__)
+        module = import_module_or_none(admin_path)
+        if module is not None and plugin.model in admin.site._registry:
+            continue
+
+        # Register the admin, since the plugin didn't do this.
+        if getattr(plugin, 'model_admin', None):
+            admin.site.register(plugin.model, plugin.model_admin)
